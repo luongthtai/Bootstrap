74269aa6b8620625867c0e69902e4b88e381a568
luongthtai <luongthetai2705@gmail.com>
2023-02-15 08:44:50 +0700

    bootstrap website

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// // slideshow icon company
+
+// let company = document.getElementById('company')
+
+// let swiper = new Swiper(company, {
+//     slidesPerView: 6,
+//     spaceBetween: 30,
+//     freeMode: true,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+// });
